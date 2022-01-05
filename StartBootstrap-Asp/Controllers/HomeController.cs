0fc416b532acd9bf6554a89ba6a286a605305aae
Nazaralieff <67953949+Nazaralieff@users.noreply.github.com>
2022-01-05 06:44:35 +0400

    Works are done

---
+++ StartBootstrap-Asp/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StartBootstrap_Asp.Data;
+using StartBootstrap_Asp.ViewModel;
+using System.Linq;
+
+namespace StartBootstrap_Asp.Controllers
+{
+    public class HomeController : Controller
+    {
+		private readonly AppDbContext _context;
+
+		public HomeController(AppDbContext context)
+		{
+			_context = context;
+		}
+        public IActionResult Index()
+        {
+            VmHome model = new VmHome()
+            {
+                sosialMedia = _context.sosialMedias.ToList(),
+                portfolios = _context.portfolios.Take(6).ToList(),
+                settings = _context.settings.FirstOrDefault()
+
+            };
+            return View(model);
+        }
+    }
+}
---
+++ StartBootstrap-Asp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StartBootstrap_Asp.Models;
+
+namespace StartBootstrap_Asp.Data
+{
+	public class AppDbContext:DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options){ }
+
+
+		public DbSet<Settings> settings { get; set; }
+		public DbSet<SosialMedia> sosialMedias { get; set; }
+		public DbSet<ContactMessage> contactMessages { get; set; }
+		public DbSet<Portfolio> portfolios { get; set; }
+	}
+}
